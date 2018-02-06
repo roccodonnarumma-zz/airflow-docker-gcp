07e1ce2c91d70251bd4108f75bca07edba92f9db
Rocco Donnarumma <roccodonnarumma@gmail.com>
2018-02-06 17:32:01 +0000

    Adding parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,41 @@
 #!/bin/bash
 
+: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Local}Executor}"
+export AIRFLOW__CORE__EXECUTOR
+
 mkdir -p ${AIRFLOW_HOME}
 cd ${AIRFLOW_HOME}
 
 mkdir -p ${AIRFLOW_HOME}/logs
 
-airflow initdb
+case "$1" in
+  webserver)
+    echo "init db"
+    airflow initdb
+    echo "starting webserver"
+    nohup airflow webserver > ${AIRFLOW_HOME}/logs/webserver.logs & 
+    if [ "${AIRFLOW__CORE__EXECUTOR}" = "LocalExecutor" ];
+    then
+      echo "starting scheduler"
+      nohup airflow scheduler > ${AIRFLOW_HOME}/logs/scheduler.logs
+    fi
+    ;;
+  scheduler)
+    sleep 10
+    nohup airflow scheduler > ${AIRFLOW_HOME}/logs/scheduler.logs
+    ;;
+  worker)
+    sleep 10
+    nohup airflow worker > ${AIRFLOW_HOME}/logs/worker.logs
+    ;;
+  flower)
+    sleep 10
+    nohup airflow flower > ${AIRFLOW_HOME}/logs/flower.logs
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
 
-nohup airflow webserver $* >> ${AIRFLOW_HOME}/logs/webserver.logs &
-nohup airflow scheduler >> ${AIRFLOW_HOME}/logs/scheduler.logs
+#nohup airflow webserver $* >> ${AIRFLOW_HOME}/logs/webserver.logs &
+#nohup airflow scheduler >> ${AIRFLOW_HOME}/logs/scheduler.logs &
